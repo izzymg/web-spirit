ec107ffc31050c2c5d2180b805d1e047cc85d267
izzymg <izzymhg@gmail.com>
2023-11-05 00:50:52 +1300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script>
+import '~/assets/global.css'
+</script>
+
+<script setup>
+const runtimeConfig = useRuntimeConfig()
+const { data, err } = useFetch(`${runtimeConfig.public.apiBase}/`)
+</script>
+
+<template>
+  <div>
+    <h1>spirit</h1>
+    <SpiritMessage v-if="err" is-error :message={err} />
+    <div v-else>
+      {{ data }}
+    </div>
+  </div>
+
+</template>
+
---
+++ components/SpiritMessage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { message, isError } = defineProps({
+  "message": String,
+  "isError": Boolean,
+})
+</script>
+
+<template>
+  <div :class="{error:isError}" class="spirit-message-wrap">
+    <span>{{ message }}</span>
+  </div>
+</template>
+  
+<style scoped>
+.spirit-message-wrap {
+  position: absolute;
+  bottom: 0;
+  left: 0; right: 0;
+  background: var(--bg-b);
+  border-radius: 4px;
+  padding: 0.8em 1em;
+}
+
+.spirit-message-wrap.error {
+  color: red;
+}
+</style>
