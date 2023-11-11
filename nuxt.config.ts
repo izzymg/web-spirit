a36134473e179a9b2d38b1ef458e97b77005ebb7
izzymg <izzymg@proton.me>
2023-11-12 09:29:20 +1300

    Config format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-icon"],
+  modules: [
+    "nuxt-icon",
+  ],
   routeRules: {
     "/v1/**": { proxy: { to: "http://localhost:3000/v1/**" } }
   }
