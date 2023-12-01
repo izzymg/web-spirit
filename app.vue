22c6d6f16c603fb1a32b747981f1691ac78338a1
izzymg <izzymg@proton.me>
2023-12-02 12:43:51 +1300

    wrap spirit in width-restricted container

--- app.vue
+++ app.vue
@@ -5,8 +5,24 @@ import '~/assets/global.css'
 <template>
   <div>
     <SpiritMessage />
-    <SpiritHeader />
-    <NuxtPage />
+    <main>
+      <div class="layout">
+        <SpiritHeader />
+        <NuxtPage />
+      </div>
+    </main>
   </div>
 </template>
 
+<style>
+main {
+  margin: 1em 0;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+
+.layout {
+  width: min(950px, 98vw);
+}
+</style>
